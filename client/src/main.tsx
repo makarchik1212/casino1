661c517f469caf9196755cc5e82a78366a34d608
makarchik1212 <makarchikloh@gmail.com>
2025-04-27 17:28:08 +0300

    Delete client directory

--- client/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-// App.tsx now contains all the necessary providers
-createRoot(document.getElementById("root")!).render(
-  <App />
-);
